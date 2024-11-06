521438cf57f171b0ed86a87716b4471dae9721e7
Devin Izair Vivas <40734852@live.napier.ac.uk>
2024-11-05 22:23:23 -0600

    Finished Lab7?

---
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -0,0 +1,30 @@
+import com.napier.sem.App;
+import com.napier.sem.City;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AppIntegrationTest
+{
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+        app.connect("localhost:33060", 10000);
+
+    }
+
+    @Test
+    void testGetEmployee()
+    {
+        City city = app.getCity(1);
+        assertEquals(city.ID, 1);
+        assertEquals(city.Name, "Kabul");
+        assertEquals(city.CountryCode, "AFG");
+        assertEquals(city.District, "Kabol");
+        assertEquals(city.Population, "1780000");
+    }
+}
